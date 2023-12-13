d0877e40960c1ae9b50e70ca6eba2eb2f52f342a
MahabubEmon <mahabubemon.cse@gmail.com>
2023-12-14 02:41:08 +0600

    object key and value find

--- object.js
+++ object.js
@@ -4,6 +4,22 @@ var phone={
     camera: '14Mp',
     stroage:'512Gb',
 }
+
+var students={
+    name :'emon',
+    totalStudent:50,
+    rollNumber:5,
+    subject:['bangle','english','physics'],
+    marks:80
+}
+
+var propertiesName='rollNumber';
+var propertiesValue=students[propertiesName];
+console.log(propertiesName,propertiesValue);
+var properties=Object.keys(students);
+var objectValue=Object.values(students);
+console.log(properties);
+console.log(objectValue);
 console.log(phone);
 phone.year=12;
 phone['omner']='emon';
