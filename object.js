a6c9e7d9bd98dd65a62be5ed6f5d30f0c631b0fd
MahabubEmon <mahabubemon.cse@gmail.com>
2023-12-14 01:29:53 +0600

    object properties add,value cng

---
+++ object.js
@@ -0,0 +1,11 @@
+var phone={
+    brand:'iphone',
+    price: 90000,
+    camera: '14Mp',
+    stroage:'512Gb',
+}
+console.log(phone);
+phone.year=12;
+phone['omner']='emon';
+phone.brand='samsung';
+console.log(phone);
