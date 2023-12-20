1b22137788ec7d61a4409795241319afb6689826
MahabubEmon <mahabubemon.cse@gmail.com>
2023-12-20 23:59:41 +0600

    math

---
+++ math.js
@@ -0,0 +1,30 @@
+
+//............Math.abs().........//
+const male=50;
+const female=54;
+const gap=Math.abs(male-female);
+if(gap<5){
+    console.log("biya kore fel");
+}
+else{
+    console.log('korish ne');
+}
+
+//............Math.pow().........//
+const taka=5
+console.log(Math.pow(taka,2));
+
+
+//............Math.round(),Math.ceil(),Math.floor........//
+const number=34.467;
+const label=Math.round(number);
+const up=Math.ceil(number);
+const down=Math.floor(number);
+console.log(label,up,down);
+
+
+//..............Math.random()..........//
+const any=Math.random()*6;
+console.log(Math.round(any));
+
+
